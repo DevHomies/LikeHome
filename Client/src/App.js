dad9b7d192a88ec01c1852d472c88af04f1c43e8
Choyee Chan Myae <103005381+ChoyeeMyae@users.noreply.github.com>
2023-03-02 22:20:52 -0800

    React setup (#5)

---
+++ Client/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+<<<<<<< HEAD
+     <h1>LikeHome</h1>
+=======
+      <h1>LikeHome</h1>
+>>>>>>> main
+    </div>
+  );
+}
+
+export default App;
