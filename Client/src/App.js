3d97bb5ab6090a7c84bc7a1364653b185fa2c3f6
Choyee Myae <choyee.myae@sjsu.edu>
2023-03-03 00:36:37 -0800

    some fix

--- Client/src/App.js
+++ Client/src/App.js
@@ -4,11 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
      <h1>LikeHome</h1>
-=======
-      <h1>LikeHome</h1>
->>>>>>> main
     </div>
   );
 }
