02c2806fc118cf14411ca80d838bd75c668b68d1
maikeruzu <54655520+maikeruzu@users.noreply.github.com>
2023-03-12 01:18:09 -0800

    Created footer components

---
+++ Client/src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Footer = () => {
+    return (
+        <div className="App">
+            <h1>I LOVE FOOTERS FEET FOOT</h1>
+        </div>
+    );
+}
+
+export default Footer;
---
+++ Client/src/components/miniFooter/minFooter.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const minFooter = () => {
+    return(
+        <div className="App">
+            <h1>I LOVE FOOTERS FEET FOOT</h1>
+        </div>
+    );
+}
+
+export default feature;
