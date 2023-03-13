84278d8dbcd8d128cd69dc0467e5d3bb3a6dd08b
xiyuanzhou <99446193+xiyuanzhou@users.noreply.github.com>
2023-03-12 21:31:06 -0700

    updated brance (#19)
    Co-authored-by: Albany Patriawan <49921782+reddesignsguy@users.noreply.github.com>
    Co-authored-by: Ryan Hedgecock <rhedgeco@gmail.com>

--- Server/catalog/urls.py
+++ Server/catalog/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('home', views.home, name='home'),
+    path('login', views.login,name='login'),
+    path('register',views.register,name='register'),
+    path('logout_view', views.logout_view,name='logout_view')
 ]
