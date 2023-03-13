3dc92672ab62dde7a49ba3fce329e5095e69c925
xiyuanzhou <xiyuan.zhou@sjsu.edu>
2023-03-12 18:17:08 -0700

    login, logout and register success.

--- README.md
+++ README.md
@@ -9,4 +9,14 @@ A mini clone of hotels.com
 - Ctrl + C to stop the server (Terminate batch job --> Y)
 
 ## Server - Run Instructions
-- ?
+- Optional intall the requirements file
+    - > pip install -r requirements.txt
+- Move into the Server directory (cd Server)
+- To run the program
+    - > python manage.py runserver
+- If the program having ERROR about "SECRET_KEY..."
+    - Try add a (.env) file inside the Server directory (maybe missing)
+    - Edit (.env) file, put this info inside the env
+        - SECRET_KEY=
+        - DEBUG=True
+- While running, Ctrl + C can stop the server
--- Server/catalog/views.py
+++ Server/catalog/views.py
@@ -1,8 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login as auth_login, logout
+from catalog import models
 
 # Create your views here.
-def index(request):
-    #testing
-    return HttpResponse("hello world")
-    
+def home(request):
+    # testing
+    user = request.user
+    reservation = models.Reservation.objects.filter(user=user)
+    context = {
+        'my_data': reservation
+    }
+    return render(request, "catalog/home.html", context=context)
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user1 = authenticate(username=username, password=password)
+        if user1:
+            auth_login(request, user1)
+            return redirect('home')
+        else:
+            msg = "can't find account"
+            return render(request, 'catalog/login.html', locals())
+    return render(request, 'catalog/login.html')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        password2 = request.POST.get("password2")
+        email = request.POST.get("email")
+        if password != password2:
+            msg = "password not match"
+            return render(request, "catalog/register.html", locals())
+        elif username == '':
+            msg = "can't be empty"
+            return render(request, "catalog/register.html", locals())
+
+        user1 = User.objects.create_user(username=username, password=password, email=email)
+        user1.save()
+
+        return redirect('home')
+    return render(request, "catalog/register.html")
+
