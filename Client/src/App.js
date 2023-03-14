58a72a1e5493e4c3414f0ab7b07a06f9686736e4
Choyee Myae <choyee.myae@sjsu.edu>
2023-03-14 14:12:32 -0700

    amenities rough draft

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,11 +1,52 @@
-import './App.css';
+//import './App.css';
+import { amenities } from "./components/amenities"
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <h1>HELLO WORLD!</h1>
+      <h3>Price</h3>
+        <div className="price-input">
+          <div className="field">
+          <span>Min</span>
+        <input type="number" className="input-min" value="100"></input>
+          </div>
+        </div>
+        <div className="price-input">
+          <div className="field">
+          <span>Max</span>
+        <input type="number" className="input-max" value="10000"></input>
+          </div>
+        </div>
+        <div className="slider">
+
+        </div>
+      
+      <h3>Amenities</h3>
+      <ul className="amenities-list">
+        {amenities.map(({name}, index) => {
+          return (
+            <li key={index}>
+              <div className ="amenities-list-item">
+                <div className="left-section">
+                  <input
+                    type="checkbox"
+                    name={amenities.map.name}
+                    value={name}
+                    />
+                    <label htmlFor={`custom-checkbox`}>{name}</label>
+                </div>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+      
+      <h3>Tag Category</h3>
+        <input
+        type="checkbox"/>
+      <label htmlFor={`custom-checkbox`}>{"Tag"}</label>
     </div>
   );
 }
 
-export default App;
+
---
+++ Client/src/components/amenities.js
@@ -0,0 +1,32 @@
+export const amenities = [
+  {
+    name: "Free Wi-Fi"
+  },
+  {
+    name: "Gym"
+  },
+  {
+    name: "Pool"
+  },
+  {
+    name: "Pet friendly"
+  },
+  {
+    name: "Hot tub"
+  },
+  {
+    name: "Breakfast included"
+  },
+  {
+    name: "Kitchen"
+  },
+  {
+    name: "Air conditioned"
+  },
+  {
+    name: "Spa"
+  },
+  {
+    name: "Restaurant"
+  }
+];
