2637042c844236636aa5a7241fdf0a15e15be3b6
Steven Vu <sssstevenvu@gmail.com>
2023-03-18 14:41:34 -0700

    Fixed functional component bug & css
    The whole jsx file was broken since functions were nested incorrectly and the export for the main functional component was removed. Got the css for the $ input field started

--- Client/src/components/Amenities/Amenities.jsx
+++ Client/src/components/Amenities/Amenities.jsx
@@ -1,7 +1,6 @@
 import './amenities.css';
 import React, { useState } from 'react';
 
-
 export const amenitiesList = [
   {
     name: "Free Wi-Fi"
@@ -34,6 +33,7 @@ export const amenitiesList = [
     name: "Restaurant"
   }
 ];
+
 export const starRatingList =[
   {
     label: '🌟',
@@ -50,36 +50,68 @@ export const starRatingList =[
   {
     label: '🌟🌟🌟🌟🌟',
   }
-]
+];
+
  export const Amenities = () =>  {
+  // 1st Edit: Have functions within the Functional Component!
+  const [count, setCount] = useState(3);
+
+  const handleShowMore = () => {
+    setCount(count + 3);
+  }
+
+  const handleShowLess = () => {
+    setCount(count - 3);
+  }
+  // End of 1st Edit
+
   return (
     <div className="Amenities">
       <h3>Price</h3>
        <div className="input-container">
-          <input type="number" name="$" placeholder="200" />
-          <input type="number" plceholder="$" />
+          <div className='input-field'>
+            <label htmlFor='lowerBound'>$</label>
+            <input type="number" name="lowerBound" placeholder="200" />
+          </div>
+          <div className='input-line'></div>
+          <div className='input-field'>
+            <label htmlFor='upperBound'>$$$</label>
+            <input type="number" name="upperBound" placeholder="500" />
+          </div>
       </div>
 
       <h3>Amenities</h3>
       <ul className="amenities-list">
-        {amenitiesList.map((name, index) => {
+        {amenitiesList.map(({name}, index) => {
           return (
             <li key={index}>
               <div className ="amenities-list-item">
                 <div className="left-section">
                   <input
                     type="checkbox"
-                    name={amenitiesList.map.name}
+                    name='custom-checkbox'
                     value={name}
-                    />
-                    <label htmlFor={`custom-checkbox`}>{name}</label>
+                  />
+                  <label htmlFor='custom-checkbox'>{name}</label>
                 </div>
               </div>
             </li>
           );
         })}
       </ul>
-      
+
+      {/* 2nd Edit: Had to add the extra {} around name in the params and moved into the functional component */}
+      {amenitiesList.slice(0, count).map(({name}, index) => (
+        <div key={index}>{name}</div>
+      ))}
+
+      {count < amenitiesList.length ? (
+        <button onClick={handleShowMore}>Show More</button>
+      ) : (
+        <button onClick={handleShowLess}>Show Less</button>
+      )}
+      {/* End of 2nd Edit */}
+
       <h3>Star Rating</h3>
       <ul className="amenities-list">
         {starRatingList.map(({label}, index) => {
@@ -100,35 +132,8 @@ export const starRatingList =[
         })}
       </ul>
     </div>
-  );
+  ); 
 }
 
-function ShowMoreLess({ amenitiesList, initialCount }) {
-  const [count, setCount] = useState(3);
-
-  const handleShowMore = () => {
-    setCount(count + 3); 
-  };
-
-  const handleShowLess = () => {
-    setCount(3); 
-  };
-
-  return (
-    <div>
-      {amenitiesList.slice(0, count).map((name, index) => (
-        <div key={index}>{name}</div>
-      ))}
-      {count < amenitiesList.length ? (
-        <button onClick={handleShowMore}>Show More</button>
-      ) : (
-        <button onClick={handleShowLess}>Show Less</button>
-      )}
-    </div>
-  );
-}
-
-export default ShowMoreLess;
-
-
-
+// 3rd Edit: You accidently REPLACED Amenities with the new function you made, So the entire component stopped being rendered
+export default Amenities;
