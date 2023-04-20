7ba22c578c5cd128946ba014ffdd53a10d109a02
jazapana <89716705+jazapana@users.noreply.github.com>
2023-04-19 21:44:56 -0700

    Details Page, shows 5 images of hotel, hotel information, all ameniti… (#29)
    Finished details page. Dummy data is commented out in the jsx file, and needs to be removed for the final product.
    
    ---------
    
    Co-authored-by: Steven Vu <sssstevenvu@gmail.com>

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import { HomePage, Login, Registerexample, AccountPage, 
-        CurrentReservations, EditReservations} from './pages';
+        CurrentReservations, EditReservations, DetailsPage } from './pages';
 
 const App = () => {
   return (
@@ -13,6 +13,9 @@ const App = () => {
         <Route path='/account' element={<AccountPage/>} />
         <Route path='/UserReservations' element={<CurrentReservations/>} />
         <Route path='/EditReservations' element={<EditReservations/>} />
+        {/* <Route path='/details' element={<DetailsPage/>} /> 
+        * This will need to be a dynamic route for each hotel
+        */}
       </Routes> 
     </div>
   );
