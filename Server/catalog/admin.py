1ced1bf22893eb19bde639ac02f9dc5506b83128
KhaNguyen04 <congkha04@gmail.com>
2023-04-28 16:09:37 -0700

    new_models_change

--- Server/catalog/admin.py
+++ Server/catalog/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from catalog.models import Reservation,HotelAmenity,Hotel,RoomAmenity,Room,Reward,Payment
+from catalog.models import Reservation,Hotel,Room,Reward,Payment
 
 # Register your models here.
 admin.site.register(Reservation)
-admin.site.register(HotelAmenity)
+# admin.site.register(HotelAmenity)
 admin.site.register(Hotel)
-admin.site.register(RoomAmenity)
+# admin.site.register(RoomAmenity)
 admin.site.register(Room)
 admin.site.register(Reward)
 admin.site.register(Payment)
